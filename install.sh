3de1c8b115a752f1928a349805af1a636b07b50d
yuyangpoi <45095089+yuyangpoi@users.noreply.github.com>
2024-09-20 22:29:45 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,23 @@
+echo "****************** Installing library ******************"
+pip install numpy
+
+pip install opencv-python
+
+pip install matplotlib
+
+pip install tqdm
+
+pip install kornia
+
+pip install rospy
+
+pip install dv_processing
+
+pip install timm
+
+pip install scipy
+
+
+
+
+
